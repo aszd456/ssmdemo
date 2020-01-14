ece950034efd1541ac1554e58f6fbd7e784c6709
itcast <itcast@itcast.com>
2020-01-14 09:27:13 +0800

    ssmdemo

---
+++ src/test/java/java8/part2/Apple.java
@@ -0,0 +1,23 @@
+package java8.part2;
+
+public class Apple {
+    private String color;
+
+    private Integer weight;
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
---
+++ src/test/java/java8/part2/Test.java
@@ -0,0 +1,31 @@
+package java8.part2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static List<Apple> filterGreenApples(List<Apple> inventory) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if ("green".equals(apple.getColor())) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+    public static List<Apple> filterApples(List<Apple> inventory, ApplePredicate p) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if (p.test(apple)) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<Apple> inventory = new ArrayList<>();
+        filterApples(inventory,(Apple apple)->"red".equals(apple.getColor()));
+    }
+}
